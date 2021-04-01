a5e10b713d0f12879d97e6aad6b87f2a807d8246
Thicc-Boi01 <tjc27882@gmail.com>
2021-04-01 16:08:43 +1100

    Initial

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Nedted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Name:");
+            String Name =Console.ReadLine();
+
+            int counter1= 0;
+
+            while(counter1 < 10){
+                int counter2 = counter1;
+
+                while(counter2 < 5){
+                    Console.Write(Name);
+                    counter2++;
+                }
+                Console.WriteLine();
+                counter1++;
+            }
+                
+                
+             Console.WriteLine("Goodbye "+Name);
+
+        }
+    }
+}
+
